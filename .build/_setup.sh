1d4e766f1a07d8ee4cf826232299610bdc88cdae
zeus <andytangeman@gmail.com>
2017-02-25 08:58:05 -0800

    Modified script execution logic

--- .build/_setup.sh
+++ .build/_setup.sh
@@ -8,65 +8,67 @@
 ANGULAR_VERS="1.6.2"
 BOOTSTRAP_VERS="3.3.7"
 
+MY_DIR=$(dirname $(readlink -f $0))
+
 # MAKE DIRS
 
-mkdir ../app
-mkdir ../app/common
-mkdir ../app/common/controllers
-mkdir ../app/common/directives
-mkdir ../app/common/filters
-mkdir ../app/common/services
+mkdir ./app
+mkdir ./app/common
+mkdir ./app/common/controllers
+mkdir ./app/common/directives
+mkdir ./app/common/filters
+mkdir ./app/common/services
 
 ## replicate common to /home, /about
 
-cp -prf ../app/common ../app/home
-cp -prf ../app/common ../app/about
+cp -prf ./app/common ./app/home
+cp -prf ./app/common ./app/about
 
 ## make asset dirs
 
-mkdir ../assets
-mkdir ../assets/img
-mkdir ../assets/css
-mkdir ../assets/js
-mkdir ../assets/lib
+mkdir ./assets
+mkdir ./assets/img
+mkdir ./assets/css
+mkdir ./assets/js
+mkdir ./assets/lib
 
-mkdir ../partials
+mkdir ./partials
 
 ## make e2e-test dirs
 
-mkdir ../e2e-tests
-mkdir ../e2e-tests/specs
+mkdir ./e2e-tests
+mkdir ./e2e-tests/specs
  
-cat _base.js > ../e2e-tests/specs/home.js
-cat _base.js > ../e2e-tests/specs/home.js
+cat $MY_DIR/_base.js > ./e2e-tests/specs/home.js
+cat $MY_DIR/_base.js > ./e2e-tests/specs/home.js
 
 # MAKE BASE FILES
 
-cat _base.js > ../app/app.js
-cat _index.html > ../index.html
-cat _basectrl.js > ../app/home/controllers/DefaultCtrl.js
-cat _base.js > ../app/home/controllers/DefaultCtrl.spec.js
-cat _base.js > ../app/home/directives/defaultDirective.html
-cat _base.js > ../app/home/directives/defaultDirective.js
-cat _base.js > ../app/home/directives/defaultDirective.spec.js
-cat _base.js > ../app/home/directives/defaultDirective.css
-cat _base.js > ../app/home/filers/defaultFilter.js
-cat _base.js > ../app/home/filters/defaultFilter.spec.js
-cat _base.js > ../app/home/services/DefaultService.js
-cat _base.js > ../app/home/services/DefaultService.spec.js
+cat $MY_DIR/_base.js > ./app/app.js
+cat $MY_DIR/_index.html > ./index.html
+cat $MY_DIR/_basectrl.js > ./app/home/controllers/DefaultCtrl.js
+cat $MY_DIR/_base.js > ./app/home/controllers/DefaultCtrl.spec.js
+cat $MY_DIR/_base.js > ./app/home/directives/defaultDirective.html
+cat $MY_DIR/_base.js > ./app/home/directives/defaultDirective.js
+cat $MY_DIR/_base.js > ./app/home/directives/defaultDirective.spec.js
+cat $MY_DIR/_base.js > ./app/home/directives/defaultDirective.css
+cat $MY_DIR/_base.js > ./app/home/filers/defaultFilter.js
+cat $MY_DIR/_base.js > ./app/home/filters/defaultFilter.spec.js
+cat $MY_DIR/_base.js > ./app/home/services/DefaultService.js
+cat $MY_DIR/_base.js > ./app/home/services/DefaultService.spec.js
 
 
 # GET ANGULAR LIBS
 
-curl -o ../assets/lib/angular.js https://code.angularjs.org/$ANGULAR_VERS/angular.js
-curl -o ../assets/lib/angular.min.js https://code.angularjs.org/$ANGULAR_VERS/angular.min.js
-curl -o ../assets/lib/angular-route.js https://code.angularjs.org/$ANGULAR_VERS/angular-route.js
-curl -o ../assets/lib/angular-route.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-route.min.js
-curl -o ../assets/lib/angular-resource.js https://code.angularjs.org/$ANGULAR_VERS/angular-resource.js
-curl -o ../assets/lib/angular-resource.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-resource.min.js
-curl -o ../assets/lib/angular-sanitize.js https://code.angularjs.org/$ANGULAR_VERS/angular-sanitize.js
-curl -o ../assets/lib/angular-sanitize.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-sanitize.min.js
+curl -o ./assets/lib/angular.js https://code.angularjs.org/$ANGULAR_VERS/angular.js
+curl -o ./assets/lib/angular.min.js https://code.angularjs.org/$ANGULAR_VERS/angular.min.js
+curl -o ./assets/lib/angular-route.js https://code.angularjs.org/$ANGULAR_VERS/angular-route.js
+curl -o ./assets/lib/angular-route.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-route.min.js
+curl -o ./assets/lib/angular-resource.js https://code.angularjs.org/$ANGULAR_VERS/angular-resource.js
+curl -o ./assets/lib/angular-resource.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-resource.min.js
+curl -o ./assets/lib/angular-sanitize.js https://code.angularjs.org/$ANGULAR_VERS/angular-sanitize.js
+curl -o ./assets/lib/angular-sanitize.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-sanitize.min.js
 
 # GET BOOTSTRAP
 
-curl -o ../assets/css/bootstrap.min.css https://maxcdn.bootstrapcdn.com/bootstrap/$BOOTSTRAP_VERS/css/bootstrap.min.css
+curl -o ./assets/css/bootstrap.min.css https://maxcdn.bootstrapcdn.com/bootstrap/$BOOTSTRAP_VERS/css/bootstrap.min.css

37d2921ed93392571a307621ce85905455c1fd38
zeus <andytangeman@gmail.com>
2017-02-25 00:02:26 -0800

    added bootstrap to download libs script

--- .build/_setup.sh
+++ .build/_setup.sh
@@ -3,7 +3,9 @@
 # Andrew Tangeman 24/Feb/2017
 
 ANGULAR_VERS="1.6.2"
+BOOTSTRAP_VERS="3.3.7"
 
+# MAKE DIRS
 
 mkdir ../app
 mkdir ../app/common
@@ -12,16 +14,21 @@ mkdir ../app/common/directives
 mkdir ../app/common/filters
 mkdir ../app/common/services
 
+## replicate common to /home, /about
+
 cp -prf ../app/common ../app/home
 cp -prf ../app/common ../app/about
+
+## make asset dirs
 
 mkdir ../assets
 mkdir ../assets/img
 mkdir ../assets/css
 mkdir ../assets/js
 mkdir ../assets/lib
-
 mkdir ../e2e-tests
+
+# MAKE BASE FILES
 
 cat _base.js > ../app/app.js
 cat _index.html > ../index.html
@@ -37,13 +44,17 @@ cat _base.js > ../app/home/services/MainService.js
 cat _base.js > ../app/home/services/MainService.spec.js
 
 
+# GET ANGULAR LIBS
 
-curl -O ../assets/lib/angular.js https://code.angularjs.org/$ANGULAR_VERS/angular.js
-curl -O ../assets/lib/angular.min.js https://code.angularjs.org/$ANGULAR_VERS/angular.min.js
-curl -O ../assets/lib/angular-route.js https://code.angularjs.org/$ANGULAR_VERS/angular-route.js
-curl -O ../assets/lib/angular-route.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-route.min.js
-curl -O ../assets/lib/angular-resource.js https://code.angularjs.org/$ANGULAR_VERS/angular-resource.js
-curl -O ../assets/lib/angular-resource.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-resource.min.js
-curl -O ../assets/lib/angular-sanitize.js https://code.angularjs.org/$ANGULAR_VERS/angular-sanitize.js
-curl -O ../assets/lib/angular-sanitize.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-sanitize.min.js
+curl -o ../assets/lib/angular.js https://code.angularjs.org/$ANGULAR_VERS/angular.js
+curl -o ../assets/lib/angular.min.js https://code.angularjs.org/$ANGULAR_VERS/angular.min.js
+curl -o ../assets/lib/angular-route.js https://code.angularjs.org/$ANGULAR_VERS/angular-route.js
+curl -o ../assets/lib/angular-route.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-route.min.js
+curl -o ../assets/lib/angular-resource.js https://code.angularjs.org/$ANGULAR_VERS/angular-resource.js
+curl -o ../assets/lib/angular-resource.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-resource.min.js
+curl -o ../assets/lib/angular-sanitize.js https://code.angularjs.org/$ANGULAR_VERS/angular-sanitize.js
+curl -o ../assets/lib/angular-sanitize.min.js https://code.angularjs.org/$ANGULAR_VERS/angular-sanitize.min.js
 
+# GET BOOTSTRAP
+
+curl -o ../assets/css/bootstrap.min.css https://maxcdn.bootstrapcdn.com/bootstrap/$BOOTSTRAP_VERS/css/bootstrap.min.css

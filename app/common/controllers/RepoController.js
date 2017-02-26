051cb43d1fc40779ce663d636f70e8ca42a52b5a
zeus <andytangeman@gmail.com>
2017-02-26 00:17:54 -0800

    added repo controller

---
+++ app/common/controllers/RepoController.js
@@ -0,0 +1,37 @@
+
+/**
+ * @file
+ */
+(function () {
+
+    "use strict";
+
+    angular
+        .module('app')
+        .controller('DefaultCtrl', DefaultCtrl);
+
+    DefaultCtrl.$inject = ['defaultService', 'logger'];
+
+    function DefaultCtrl(defaultService, logger) {
+        var vm = this;
+        vm.load = load;
+        vm.data = [];
+        vm.title = 'Default';
+
+        activate();
+
+        function activate() {
+            return getData().then(function() {
+                logger.info('Activated Default View');
+            });
+        }
+
+        function getData() {
+            return defaultService.getData().then(function(data) {
+                vm.data = data;
+                return vm.data;
+            });
+        }
+    }
+
+})();

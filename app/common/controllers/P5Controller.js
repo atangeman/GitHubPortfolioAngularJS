46a1c6cb8105827fccc26804b763bd80e3fa450e
zeus <andytangeman@gmail.com>
2017-03-20 10:51:26 -0700

    still mistake

--- app/common/controllers/P5Controller.js
+++ app/common/controllers/P5Controller.js
@@ -1,27 +1,31 @@
-
 /**
  * @file
  */
 (function () {
-
     "use strict";
+    angular.module('app').controller('P5Controller', P5Controller);
+    P5Controller.$inject = ['p5Service', 'repoService'];
 
-    angular
-        .module('app')
-        .controller('P5Controller', P5Controller);
-
-    P5Controller.$inject = ['p5Service'];
-
-    function P5Controller(p5Service) {
+    function P5Controller(p5Service, repoService) {
         var vm = this;
-        vm.activate = activate; 
+        vm.activate = activate;
+        var repos = {};
+        vm.apiURL = 'https://api.github.com';
+        vm.repoURL = '/users/atangeman/repos';
+        vm.getRepos = getRepos;
+        vm.sketch = p5Service(this);
         
-        activate();
+        activate();     
         
         function activate() {
-            return p5Service(this);
+            var url = vm.apiURL + vm.repoURL;
+        }
+
+        function getRepos(url) {
+            return repoService.getReposByLanguage(url).then(function(data) {
+                vm.repos = data;
+                return vm.repos;
+            });
         }
     }
-
-        
 })();
--- app/common/services/repoService.js
+++ app/common/services/repoService.js
@@ -3,29 +3,63 @@
  */
 (function () {
     "use strict";
-    
-    angular
-        .module('app')
-        .factory('repoService', repoService);
-    
+    angular.module('app').factory('repoService', repoService);
     repoService.$inject = ['$http'];
-    
+
     function repoService($http) {
         return {
             getRepos: getRepos
-         };
+            , getReposByLanguage: getReposByLanguage
+        };
         /////////////
         function getRepos(url) {
-            return $http.get('https://api.github.com/users/atangeman/repos')
-                .then(getReposComplete)
-                .catch(getReposFailed);
+            return $http.get('https://api.github.com/users/atangeman/repos?access_token=c6e54af38dccdef074ee0cfa3562dc2f3d9f2f97').then(getReposComplete).catch(getReposFailed);
 
             function getReposComplete(response) {
                 return response.data;
             }
 
             function getReposFailed(error) {
-               /* logger.error('XHR Failed for getAvengers.' + error.data); */
+                /* logger.error('XHR Failed for getAvengers.' + error.data); */
+            }
+        }
+
+        function getReposByLanguage(url) {
+            return $http.get('https://api.github.com/users/atangeman/repos?access_token=c6e54af38dccdef074ee0cfa3562dc2f3d9f2f97').then(getReposComplete).catch(getReposFailed);
+
+            function getReposComplete(response) {
+                var data = response.data;
+                var returnObjArr = []; 
+                var returnObj = {};
+                for (var i = 0; i < data.length; ++i) {
+                    if (returnObj[data[i].language] != null) {
+                        var fData = formatRepo(data[i]);
+                        returnObj[data[i].language].items.push(fData);
+                    }
+                    else {
+                        returnObj[data[i].language] = {
+                            language: data[i].language,
+                            alias: "T",
+                            size: 90,
+                            posX: null,
+                            items: []
+                        }
+                    }
+                }
+                return Object.values(returnObj);
+            }
+
+            function formatRepo(data) {
+                return {
+                    id: data.id,
+                    name: data.name,
+                    description: data.description,
+                    url: data.url
+                }
+            }
+
+            function getReposFailed(error) {
+                /* logger.error('XHR Failed for getAvengers.' + error.data); */
             }
         }
 
